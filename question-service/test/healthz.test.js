7f6d28bc5c2d4b1b7cdf1702ff8b2de58764a9db
pzl111 <phuazailian@gmail.com>
2024-11-07 21:52:30 +0800

    chore: Enable tests

--- question-service/test/healthz.test.js
+++ question-service/test/healthz.test.js
@@ -1,13 +1,13 @@
-// import app from '../index.js';
-// import request from 'supertest';
-// import assert from 'assert';
+import app from '../index.js';
+import request from 'supertest';
+import assert from 'assert';
 
-// describe('Test healthz', function () {
-//     it('Respond with 200 status code with success message', async function () {
-//         const res = await request(app)
-//             .get('/healthz')
-//             .expect(200)
-//             .expect('Content-Type', /json/);
-//         assert.equal(res.body.message, 'Connected to /healthz route of question-service', 'Response message does not match');
-//     });
-// });
+describe('Test healthz', function () {
+    it('Respond with 200 status code with success message', async function () {
+        const res = await request(app)
+            .get('/healthz')
+            .expect(200)
+            .expect('Content-Type', /json/);
+        assert.equal(res.body.message, 'Connected to /healthz route of question-service', 'Response message does not match');
+    });
+});

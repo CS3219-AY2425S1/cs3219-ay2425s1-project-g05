245a7460d281fed14cf6b4731f5f23e815542091
George Tay <georgetay71@gmail.com>
2024-11-07 17:50:27 +0800

    chore: update cors and fix docker files

--- history-service/middlewares/cors.js
+++ history-service/middlewares/cors.js
@@ -1,14 +1,20 @@
-import cors from "cors";
+import cors from 'cors';
 
 const allowedOrigins = [
-  "http://localhost:5173", // frontend dev
-  "http://peerprep.s3-website-ap-southeast-1.amazonaws.com", // frontend prod
-  "http://peerprep-frontend-bucket.s3-website-ap-southeast-1.amazonaws.com", // frontend staging
-  "http://localhost:8000",
-  "http://localhost:8001",
-  "http://localhost:8002",
-  "http://localhost:8004",
-  "http://localhost:8005",
+  'http://localhost:5173', // frontend dev
+  'http://peerprep.s3-website-ap-southeast-1.amazonaws.com', // frontend prod
+  'http://peerprep-frontend-bucket.s3-website-ap-southeast-1.amazonaws.com', // frontend staging
+  'https://01678sag05.execute-api.ap-southeast-1.amazonaws.com', // online api gateway
+  'http://PeerPrepALB-705702575.ap-southeast-1.elb.amazonaws.com', // online load balancer
+  'http://redis-001.mrdqdr.0001.apse1.cache.amazonaws.com:6379', // online redis instance
+  'http://localhost:8000',
+  'http://localhost:8001', // user service
+  'http://localhost:8002', // matching service
+  'http://localhost:8003', // question service
+  'http://localhost:8004', // collaboration service
+  'http://localhost:8005',
+  'http://localhost:8006',
+  'http://localhost:8007',
 ];
 
 // PORT 8000 - FRONTEND
@@ -21,8 +27,15 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: allowedOrigins,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'Cookie',
+    'X-Amz-Date',
+    'X-Api-Key',
+    'X-Amz-Security-Token',
+  ],
   credentials: true,
 };
 
